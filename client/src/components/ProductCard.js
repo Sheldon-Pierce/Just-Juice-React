fe8af6d04b2275ac84db4c586f366992ee24edb6
Sheldon Pierce <Pierce55@icloud.com>
2023-03-02 08:39:16 -0800

    add cart to item and remove

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -12,32 +12,70 @@ import {
   Link,
   HStack,
   Text,
+  useToast
 } from '@chakra-ui/react';
 import { FiShoppingCart } from 'react-icons/fi';
 import { Link as ReactLink } from 'react-router-dom';
 import { StarIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCartItem } from '../redux/actions/cartActions';
+
+const Rating = ({ rating, numberOfReviews }) => {
+  const { iconSize } = useState('14px');
+  return (
+    <Flex>
+      <HStack spacing='2px'>
+        <StarIcon size={iconSize} w='14px' color={'orange.500'} />
+        <StarIcon
+          size={iconSize}
+          w='14px'
+          color={rating >= 2 ? 'orange.500' : 'gray.200'}
+        />
+        <StarIcon
+          size={iconSize}
+          w='14px'
+          color={rating >= 3 ? 'orange.500' : 'gray.200'}
+        />
+        <StarIcon
+          size={iconSize}
+          w='14px'
+          color={rating >= 4 ? 'orange.500' : 'gray.200'}
+        />
+        <StarIcon
+          size={iconSize}
+          w='14px'
+          color={rating >= 5 ? 'orange.500' : 'gray.200'}
+        />
+      </HStack>
+      <Text fontSize={'md'} fontWeight='bold' ml='4px'>
+        {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review' : 'Reviews'}`}
+      </Text>
+    </Flex>
+  );
+};
+
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const cartInfo = useSelector((state) => state.cart);
+  const {cart} = cartInfo;
+
+  const addItem = (id) => {
+    if(cart.some((cartItem) => cartItem.id === id)) {
+      toast({
+        description: 'This item is already in your cart. Go to your cart to change the amount.',
+        status: 'error',
+        isClosable: true,
+      });
+    } else {
+      dispatch(addCartItem(id, 1))
+      toast({description: 'Item has been added.', status: 'success', isClosable: true})
+    }
+  }
 
 
-const Rating = ({rating, numberOfReviews }) => {
-    const {iconSize} = useState('14px');
-    return (
-        <Flex>
-            <HStack spacing='2px'>
-                <StarIcon size={iconSize} w='14px' color={'orange.500'} />
-                <StarIcon size={iconSize} w='14px' color={rating >= 2 ? 'orange.500': 'gray.200'} />
-                <StarIcon size={iconSize} w='14px' color={rating >= 3 ? 'orange.500': 'gray.200'} />
-                <StarIcon size={iconSize} w='14px' color={rating >= 4 ? 'orange.500': 'gray.200'} />
-                <StarIcon size={iconSize} w='14px' color={rating >= 5 ? 'orange.500': 'gray.200'} />
-            </HStack>
-            <Text fontSize={'md'} fontWeight='bold' ml='4px'>
-                {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review' : 'Reviews'}`}
-            </Text>
-        </Flex>
-    )
-}
 
-function ProductCard({ product }) {
   return (
     <Stack
       p='2'
@@ -97,7 +135,10 @@ function ProductCard({ product }) {
         </Link>
       </Flex>
       <Flex justifyContent={'space-between'} alignContent='center' py='2'>
-        <Rating rating={product.rating} numberOfReviews={product.numberOfReviews} />
+        <Rating
+          rating={product.rating}
+          numberOfReviews={product.numberOfReviews}
+        />
       </Flex>
       <Flex justify={'space-between'}>
         <Box fontSize={'2xl'} color={useColorModeValue('gray.800', 'white')}>
@@ -117,13 +158,13 @@ function ProductCard({ product }) {
             variant={'ghost'}
             display='flex'
             disabled={product.stock <= 0}
-          >
+            onClick={() => addItem(product._id)}>
             <Icon as={FiShoppingCart} h={7} w={7} alignSelf='center' />
           </Button>
         </Tooltip>
       </Flex>
     </Stack>
   );
-};
+}
 
 export default ProductCard;
---
+++ client/src/redux/actions/cartActions.js
@@ -0,0 +1,33 @@
+import axois from 'axios';
+import { setLoading, setError, cartItemAdd, cartItemRemoval } from '../slices/cart';
+
+export const addCartItem = (id, qty) => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const { data } = await axois.get(`/api/products/${id}`);
+    const itemToAdd = {
+      id: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      stock: data.stock,
+      qty,
+    };
+    dispatch(cartItemAdd(itemToAdd));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : 'An unexpected error has occured. Please try again later.'
+      )
+    );
+  }
+};
+
+export const removeCartItem = (id) => async (dispatch) => {
+  dispatch(setLoading(true));
+  dispatch(cartItemRemoval(id));
+}
