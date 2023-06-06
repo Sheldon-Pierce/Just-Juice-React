23d94ca430b881e63eae6e33586cc169d00ded55
Sheldon Pierce <Pierce55@icloud.com>
2023-06-06 15:14:45 -0700

    Fixing Checkout Total

--- client/src/components/CheckoutOrderSummary.js
+++ client/src/components/CheckoutOrderSummary.js
@@ -12,48 +12,87 @@ import {
 } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as ReactLink } from 'react-router-dom';
-import {PhoneIcon, EmailIcon, ChatIcon} from '@chakra-ui/icons'
-import {createOrder} from '../redux/actions/orderActions';
+import { PhoneIcon, EmailIcon, ChatIcon } from '@chakra-ui/icons';
+import { createOrder } from '../redux/actions/orderActions';
 import { useEffect, useState, useCallback } from 'react';
 import CheckoutItem from './CheckoutItem';
 
 const CheckoutOrderSummary = () => {
-    const colorMode = mode('gray.600', 'gray.400');
-    const cartItems = useSelector((state) => state.cart);
-    const { cart, subtotal, expressShipping } = cartItems;
+  const colorMode = mode('gray.600', 'gray.400');
+  const cartItems = useSelector((state) => state.cart);
+  const { cart, subtotal, expressShipping } = cartItems;
 
-    const user = useSelector((state) => state.user);
-    const { userInfo } = user;
+  const user = useSelector((state) => state.user);
+  const { userInfo } = user;
 
-    const shippingInfo = useSelector((state) => state.order);
-    const { error, shippingAddress } = cartItems;
-    const [buttonDisabled, setButtonDisabled] = useState(false);
-    const dispatch = useDispatch();
+  const shippingInfo = useSelector((state) => state.order);
+  const { error, shippingAddress } = cartItems;
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const dispatch = useDispatch();
 
-    const shipping = useCallback(
-        () => (expressShipping === 'true' ? 14.99 : subtotal <= 1000 ? 4.99 : 0),
-        [expressShipping, subtotal]
-    );
+  const shipping = useCallback(
+    () => (expressShipping === 'true' ? 14.99 : subtotal <= 1000 ? 4.99 : 0),
+    [expressShipping, subtotal]
+  );
 
-    const total = useCallback(
-        () => Number(shipping() === 0 ? Number(subtotal): Number(subtotal) + shipping()).toFixed(2),
-        [shipping, subtotal]
-    );
+  const total = useCallback(
+    () =>
+      Number(
+        shipping() === 0 ? Number(subtotal) : Number(subtotal) + shipping()
+      ).toFixed(2),
+    [shipping, subtotal]
+  );
 
-    const onPaymentSucess = () => {
-        alert('order success')
-    };
+  const onPaymentSucess = () => {
+    alert('order success');
+  };
 
-    const onPaymentError = () => {
-        alert('order error');
-    };
+  const onPaymentError = () => {
+    alert('order error');
+  };
 
-    return <Stack spacing='8' rounded='xl' padding='8' width='full'>
-        <Heading size='md'>Order Summary</Heading>
-        {cart.map((item) => (
-            <CheckoutItem key={item.id} cartItem={item}/>
-        ))}
-    </Stack>;
+  return (
+    <Stack spacing='8' rounded='xl' padding='8' width='full'>
+      <Heading size='md'>Order Summary</Heading>
+      {cart.map((item) => (
+        <CheckoutItem key={item.id} cartItem={item} />
+      ))}
+
+      <Stack spacing='6'>
+        <Flex justify='space-between'>
+          <Text fontWeight='medium' color={colorMode}>
+            Subtotal
+          </Text>
+          <Text fontWeight='medium' color={colorMode}>
+            {subtotal}
+          </Text>
+        </Flex>
+        <Flex justify='space-between'>
+          <Text fontWeight='medium' color={colorMode}>
+            Shipping
+          </Text>
+          <Text fontWeight='medium' color={colorMode}>
+            {shipping() === 0 ? (
+              <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='green'>
+                Free
+              </Badge>
+            ) : (
+              `$${shipping()}`
+            )}
+          </Text>
+        </Flex>
+
+        <Flex justify='space-between'>
+          <Text  fontSize='lg' fontWeight='semibold'>
+            Total
+          </Text>
+          <Text fontSize='xl' fontWeight='extrabold' color={colorMode}>
+            ${Number(total())}
+          </Text>
+        </Flex>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default CheckoutOrderSummary;
