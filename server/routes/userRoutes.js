ef129cfa258e549a8f40239e1a068b9066d4c940
Sheldon Pierce <Pierce55@icloud.com>
2023-06-18 20:35:43 -0700

    Getting ready to render site to Render!!

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -26,7 +26,7 @@ const loginUser = asyncHandler(async (req, res) => {
       createdAt: user.createdAt,
     });
   } else {
-    res.status(401);
+    res.status(401).send('Invalid email or password.');
     throw new Error('Invalid email or password.');
   }
 });
@@ -36,7 +36,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email });
   if (userExists) {
-    res.status(400);
+    res.status(400).send('We already have an account with that email address.');
     throw new Error('We already have an account with that email address.');
   }
 
@@ -55,7 +55,7 @@ const registerUser = asyncHandler(async (req, res) => {
       token: genToken(user._id),
     });
   } else {
-    res.json(400);
+    res.json(400).send('Invalid user data');
     throw new Error('Invalid user data');
   }
 });
