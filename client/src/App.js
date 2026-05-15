8e928f482cf7ceeebf78afcb2e6e0955ef6f8a9b
Sheldon Pierce <pierce55@icloud.com>
2026-05-15 16:50:29 -0700

    feat(theme): wire custom Chakra theme, drop dark mode, self-host fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import theme from './theme';
 import Navbar from './components/Navbar';
-import ProductsScreen from './screens/ProductsScreen';
-import CartScreen from './screens/CartScreen';
-import ProductScreen from './screens/ProductScreen';
 import { Footer } from './components/Footer';
 import { LandingScreen } from './screens/LandingScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegistrationScreen from './screens/RegistrationScreen';
 import ProfileScreen from './screens/ProfileScreen';
@@ -16,22 +17,23 @@ import OrderSuccessScreen from './screens/OrderSuccessScreen';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Router>
         <Navbar />
         <main>
           <Routes>
-            <Route path='/' element={<LandingScreen />}></Route>
-            <Route path='/products' element={<ProductsScreen />}></Route>
-            <Route path='/product/:id' element={<ProductScreen />}></Route>
-            <Route path='/cart' element={<CartScreen />}></Route>
-            <Route path='/login' element={<LoginScreen />}></Route>
-            <Route path='/registration' element={<RegistrationScreen />}></Route>
-            <Route path='/profile' element={<ProfileScreen />}></Route>
-            <Route path='/checkout' element={<CheckoutScreen />}></Route>
-            <Route path='/your-orders' element={<YourOrdersScreen />}></Route>
-            <Route path='/admin-console' element={<AdminConsoleScreen />}></Route>
-            <Route path='/order-success' element={<OrderSuccessScreen />}></Route>
+            <Route path='/' element={<LandingScreen />} />
+            <Route path='/products' element={<ProductsScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/registration' element={<RegistrationScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
+            <Route path='/checkout' element={<CheckoutScreen />} />
+            <Route path='/your-orders' element={<YourOrdersScreen />} />
+            <Route path='/admin-console' element={<AdminConsoleScreen />} />
+            <Route path='/order-success' element={<OrderSuccessScreen />} />
           </Routes>
         </main>
         <Footer />
